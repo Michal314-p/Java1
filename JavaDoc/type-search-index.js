8c28aab767d95520174e95e136e6ca613444385f
Michal314-p <59016764+Michal314-p@users.noreply.github.com>
2020-03-23 03:05:54 +0100

    Add JavaDoc
    JavaDoc of cw1

---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"cw_sort","l":"AbstractFloatSorter"},{"p":"cw_sort","l":"AbstractIntSorter"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"cw_sort","l":"Counting"},{"p":"cw_sort","l":"FloatElement"},{"p":"cw_sort","l":"IElement"},{"p":"cw_sort","l":"Insert"},{"p":"cw_sort","l":"IntElement"},{"p":"cw_sort","l":"Main"},{"p":"cw_sort","l":"Quick"}]
